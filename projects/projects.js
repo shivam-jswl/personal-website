b89eff9b69e7d6d5a01a74ee009dbcd82fd32275
Shivam <kaalraavan@gmail.com>
2023-09-17 18:07:03 +0530

    v1.0

--- projects/projects.js
+++ projects/projects.js
@@ -3,7 +3,7 @@ function imageViewer(path, quata){
     document.getElementById("image_place").src = path;
     document.getElementById("image_show_id").style.display = "flex";
     document.body.style.overflow = "hidden";
-
+    document.getElementById("caption_insert").innerHTML = quata;
 }
 
 function closeme(){
